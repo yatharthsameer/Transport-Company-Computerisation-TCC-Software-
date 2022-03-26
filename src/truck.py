b9bdfa8d0f78adf7e19b3875b6731d4469af3e61
yatharthsameer <ytharthsmr@gmail.com>
2022-03-26 11:42:24 +0530

    hi

--- src/truck.py
+++ src/truck.py
@@ -1,2 +1,14 @@
 import pymongo
 import utility
+
+
+class Truck:
+    def __init__(self, numberPlate, CurrentLocation, DriverAsigned,Driver) -> None:
+        self.numberPlate = numberPlate
+        self.CurrentLocation = CurrentLocation
+        self.DriverAsigned = DriverAsigned
+        self.Driver = Driver
+        
+
+    def convertToDictAndUpload(self) -> None:
+        utility.truckDB.insert_one({'numberPlate': self.numberPlate, 'CurrentLocation': self.CurrentLocation, 'DriverAsigned': self.DriverAsigned, 'Driver': self.Driver})
