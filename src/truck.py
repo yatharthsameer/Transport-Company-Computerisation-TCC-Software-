bcf61a40de7e740afa4e215fea38c11089fd4848
skullVoid <93020016+skullVoid@users.noreply.github.com>
2022-03-26 17:29:54 +0530

    truck update

--- src/truck.py
+++ src/truck.py
@@ -3,17 +3,25 @@ import utility
 
 
 class Truck:
-    def __init__(self, numberPlate, CurrentLocation, DriverAsigned,Driver) -> None:
+    def __init__(self, numberPlate, CurrentLocation, Driver, driverNumber) -> None:
         self.numberPlate = numberPlate
         self.CurrentLocation = CurrentLocation
-        self.DriverAsigned = DriverAsigned
         self.Driver = Driver
+        self.driverNumber = driverNumber
         
 
     def convertToDictAndUpload(self) -> None:
+        id = utility.settings.find_one({'_id': 0})['TruckID']
+        utility.settings.update_one({'_id': 0}, {'$set': {'TruckID': id + 1}})
         utility.truckDB.insert_one({
-            'numberPlate': self.numberPlate, 
-            'CurrentLocation': self.CurrentLocation, 
-            'DriverAsigned': self.DriverAsigned, 
-            'Driver': self.Driver})
+            '_id': id,
+            'Last Dispatch Date': 'NA',
+            'Total Consignments Delivered': 0,
+            'Current Driver': self.Driver, 
+            'Location': self.CurrentLocation,
+            'Number Plate': self.numberPlate,
+            'Driver Phone': self.driverNumber,
+            'Consignments Loaded': [],
+            'Status': 'Available',
+            'Next Destination': 'NA'})
         
