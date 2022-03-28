27316ec5138f080f8fcc8145d34e5df38bbfd141
skullVoid <93020016+skullVoid@users.noreply.github.com>
2022-03-28 12:57:17 +0530

    minor changes

--- src/mail.py
+++ src/mail.py
@@ -1,13 +1,3 @@
-'''
-import smtplib
-
-email = "atulyaankitsharma@kgpian.iitkgp.ac.in"
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-server.login("grp10diy@gmail.com", "diyproject")
-server.sendmail("testpython@test.com", email, "Hello")
-server.quit()
-'''
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
